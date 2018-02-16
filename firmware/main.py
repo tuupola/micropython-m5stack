e49bcb929f5de84a4e90f87201356589c9aeb62d
Mika Tuupola <tuupola@appelsiini.net>
2018-02-16 23:10:24 +0700

    Add subtle sound effect when pressing and releasing a button

--- firmware/main.py
+++ firmware/main.py
@@ -9,10 +9,12 @@
 #   https://github.com/tuupola/micropython-m5stack
 #
 
-from machine import Pin, SPI # pylint: disable=import-error
-from input import DigitalInput # pylint: disable=import-error
-import display # pylint: disable=import-error
-import m5stack # pylint: disable=import-error
+#pylint: disable=import-error
+from machine import Pin, SPI
+from input import DigitalInput
+import display
+import m5stack
+#pylint: enable=import-error
 
 tft = m5stack.Display()
 
@@ -28,26 +30,42 @@ tft.text(tft.CENTER, tft.LASTY, "                6mmm9  \n")
 tft.text(tft.CENTER, tft.LASTY, "                       \n")
 tft.text(tft.CENTER, tft.LASTY, "https://appelsiini.net/")
 
-a = m5stack.ButtonA(
-    callback=lambda pin, pressed: tft.text(
-        tft.CENTER, tft.LASTY, "> Button A " + ("pressed.     " if pressed else "released.    ")
-    )
-)
+def button_hander_a(pin, pressed):
+    if pressed is True:
+        tft.text(
+            tft.CENTER, tft.LASTY, "> Button A pressed.     "
+        )
+        m5stack.tone(1800, duration=10, volume=1)
+    else:
+        tft.text(
+            tft.CENTER, tft.LASTY, "> Button A released.    "
+        )
+        m5stack.tone(1300, duration=10, volume=1)
 
-b = m5stack.ButtonB(
-    callback=lambda pin, pressed: tft.text(
-        tft.CENTER, tft.LASTY, "> Button B " + ("pressed.     " if pressed else "released.    ")
-    )
-)
+def button_hander_b(pin, pressed):
+    if pressed is True:
+        tft.text(
+            tft.CENTER, tft.LASTY, "> Button B pressed.     "
+        )
+        m5stack.tone(2000, duration=10, volume=1)
+    else:
+        tft.text(
+            tft.CENTER, tft.LASTY, "> Button B released.    "
+        )
+        m5stack.tone(1500, duration=10, volume=1)
 
-def button_hander(pin, pressed):
+def button_hander_c(pin, pressed):
     if pressed is True:
         tft.text(
             tft.CENTER, tft.LASTY, "> Button C pressed.     "
         )
+        m5stack.tone(2200, duration=10, volume=1)
     else:
         tft.text(
             tft.CENTER, tft.LASTY, "> Button C released.    "
         )
+        m5stack.tone(1800, duration=10, volume=1)
 
-c = m5stack.ButtonC(callback=button_hander)
+a = m5stack.ButtonA(callback=button_hander_a)
+b = m5stack.ButtonB(callback=button_hander_b)
+c = m5stack.ButtonC(callback=button_hander_c)
